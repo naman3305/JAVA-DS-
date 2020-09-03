89af5f6432e41d447803f5a200ef39578f99d617
naman3305 <59595534+naman3305@users.noreply.github.com>
2020-09-04 00:57:02 +0530

    Update binary_search.java

--- binary_search.java
+++ binary_search.java
@@ -1,9 +1,36 @@
-run:
-Enter the size of array 
-5
-Enter the element of the array
-1 2 3 4 5
-Enter the element to be searched
-4
-Element found at index 3
-BUILD SUCCESSFUL (total time: 17 seconds)
+import java.util.*;
+
+class binary_search { 
+   static int binarySearch(int arr[], int l, int r, int x) 
+    { 
+        if (r >= l) { 
+            int mid = l + (r - l) / 2; 
+            if (arr[mid] == x) 
+                return mid;
+            if (arr[mid] > x) 
+                return binarySearch(arr, l, mid - 1, x); 
+            return binarySearch(arr, mid + 1, r, x); 
+        }
+        return -1; 
+    } 
+  
+    public static void main(String args[]) 
+    { 
+          Scanner s=new Scanner(System.in);
+        System.out.println("Enter the size of array ");
+        int n=s.nextInt();
+        int arr[] =new int[n];
+         System.out.println("Enter the element of the array");
+    for(int i=0;i<n;i++)
+    {
+        arr[i]=s.nextInt();
+    }
+    System.out.println("Enter the element to be searched");
+        int x=s.nextInt();
+        int result = binarySearch(arr, 0, n - 1, x); 
+        if (result == -1) 
+            System.out.println("Element not present"); 
+        else
+            System.out.println("Element found at index " + result); 
+    } 
+} 
