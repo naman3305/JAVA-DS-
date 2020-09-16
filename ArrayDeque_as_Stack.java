dadcf6ebb66a284bfe44946cceacbe6a49bb3e1c
naman3305 <59595534+naman3305@users.noreply.github.com>
2020-09-16 14:21:59 +0530

    Add files via upload

---
+++ ArrayDeque_as_Stack.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class ArrayDeque_as_Stack {
+      public static void main(String args[])
+      {
+       ArrayDeque<Integer> ad=new ArrayDeque<Integer>();
+       ad.push(10);
+       ad.push(20);
+       System.out.println(ad.peek());
+       System.out.println(ad.pop());
+       System.out.println(ad.peek());
+       System.out.println(ad.poll());//pop throw error in empty queue.
+       ad.push(40);
+       System.out.println(ad.peek());
+      }  
+}
