3d2173439fb183f28b653d24ca0818c9c4d3359d
naman3305 <59595534+naman3305@users.noreply.github.com>
2020-10-19 16:25:30 +0530

    Add files via upload

---
+++ BSTinsertIterative.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class BSTinsertIterative {
+    static Node insert(Node root,int x)
+    {
+        Node temp=new Node(x);
+        Node parent=null,curr=root;
+        while(curr!=null)
+        {
+            parent=curr;
+            if(curr.key>x)
+                curr=curr.left;
+            else if(curr.key<x)
+                curr=curr.right;
+            else
+                return root;
+        }
+        if(parent==null)
+            return temp;
+       else if(parent.key>x)
+            parent.left=temp;
+        else
+            parent.right=temp;
+        return root;
+    }
+    public static void main(String args[])
+    {
+        Node root=null;
+        insert(root,10);
+    }
+}
