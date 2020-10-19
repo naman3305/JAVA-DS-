78b20807e831d3c134e7b4f2f13008f03bd24137
naman3305 <59595534+naman3305@users.noreply.github.com>
2020-10-19 16:25:14 +0530

    Add files via upload

---
+++ BSTinsert.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class BSTinsert {
+    static Node insert(Node root,int x)
+    {
+        if(root==null)
+            return new Node(x);
+        if(root.key>x)
+            root.left=insert(root.left,x);
+        else if(root.key<x)
+            root.right=insert(root.right,x);
+        return root;
+    }
+    public static void main(String args[])
+    {
+        Node root=null;
+        insert(root,10);
+    }
+}
