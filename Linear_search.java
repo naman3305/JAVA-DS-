61e3cd84483ae70996214f9194a958bdbcc471db
naman3305 <59595534+naman3305@users.noreply.github.com>
2021-01-06 20:06:00 +0530

    Update Linear_search.java

--- Linear_search.java
+++ Linear_search.java
@@ -1,7 +1,8 @@
 import java.util.*;
 class liner_search      
 {
-    public static void main(String[] args){
+    public static void main(String[] args)
+    {
         Scanner s=new Scanner(System.in);
         System.out.println("Enter the size of array ");
         int n=s.nextInt();
